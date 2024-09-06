5103ce3a4d8252053ce05aab263603b8062759b5
Kenneth <katoapanta4@espe.edu.ec>
2024-09-06 00:41:40 -0500

    Creado Invencio

---
+++ layouts/admin_menu.php
@@ -0,0 +1,61 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Inicio</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>Gestión de Usuarios</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Gestionar grupos</a> </li>
+      <li><a href="users.php">Gestionar usuarios</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Categorías</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Productos</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Gestionar productos</a> </li>
+       <li><a href="add_product.php">Agregar productos</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Archivos multimedia</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-credit-card"></i>
+       <span>Ventas</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="sales.php">Gestionar ventas</a> </li>
+         <li><a href="add_sale.php">Agregar ventas</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-duplicate"></i>
+       <span>Informe de ventas</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="sales_report.php">Ventas por fechas</a></li>
+        <li><a href="monthly_sales.php">Ventas mensuales</a></li>
+        <li><a href="daily_sales.php">Ventas diarias</a> </li>
+      </ul>
+  </li>
+</ul>
